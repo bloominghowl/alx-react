0f8c8ee0d77d80e3d00f0a05db33f8190c968e17
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2024-05-05 18:38:21 +0000

    Update notifications.js

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,7 +1,27 @@
-import notificationData from '../../../../notifications.json';
+import * as notificationsData from '../../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-export default function getAllNotificationsByUser(userId) {
-	return notificationData
-		.filter((notification) => notification.author.id === userId)
-		.map((notification) => notification.context);
-}
+export const getAllNotificationsByUser = (userId) => {
+  return notificationsData.default
+    .filter((item) => item.author.id === userId)
+    .map(({ context }) => context);
+};
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity(
+  'messages',
+  {},
+  {
+    idAttribute: 'guid',
+  }
+);
+
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+const normalized = normalize(notificationsData.default, [notification]);
+
+export { normalized };
