fe7bad143950c3a1cc8e91a3fcf7092aef1e167a
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2024-05-12 18:21:36 +0000

    Update notificationSelector.js

--- 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
+++ 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
@@ -1,13 +1,9 @@
-export const filterTypeSelected = (state) => {
-  return state.get("filter");
-};
-export const getNotifications = (state) => {
-  return state.get("notifications");
-};
-export const getUnreadNotifications = (state) => {
-  const notifications = state.get("notifications");
-  const filtered = notifications.filter(
-    (value, key) => value.get("isRead") === true
-  );
-  return filtered;
+export const getListCourses = (state) => {
+  const courses = state.courses;
+
+  if (courses) {
+    return courses.valueSeq();
+  }
+
+  return courses;
 };
